2d51e5a42c64b6cbebae248e13666a7450964bef
bangbaht1669 <bangbaht1669@gmail.com>
2020-12-14 18:33:25 +0700

    addnew

--- pacjava.java
+++ pacjava.java
@@ -1,5 +1,11 @@
 class pacjava{
     public static void main(String[] args) {
-        System.out.print("packyyyyyy");
+        for(int i = 0;i<10;i+=2){
+            for(int p = 0;p<i;p+=2){
+            System.out.print("*");
+            }
+        System.out.println();
+        }
+        
     }
 }
---
+++ peramit.py
@@ -0,0 +1,12 @@
+    
+num  = int(input("Input :"))
+j = num-1
+for p in range(1,num+num,2):
+    print(" "*j+p*"*")
+    j-=1
+
+
+   
+
+    
+    
